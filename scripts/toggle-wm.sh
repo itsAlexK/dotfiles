cd7a436bb2f4899328239bcfdcc5d8e6efb1e2ea
itsAlexK <contactkaichen@gmail.com>
2019-06-08 21:03:25 -0400

    update

---
+++ scripts/toggle-wm.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+set -euxo pipefail
+
+COMMAND=$1
+
+if [[ "$COMMAND" =~ ^(start|stop|restart)$ ]]; then
+    brew services "${COMMAND}" chunkwm
+    brew services "${COMMAND}" skhd
+else
+    echo "${COMMAND} is not a valid option"
+fi
