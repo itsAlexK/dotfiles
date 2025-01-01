2ed334ca4a73494971fe544d1abae3deaeb3ccd2
itsAlexK <contactkaichen@gmail.com>
2024-12-31 21:24:29 -0500

    Add additonal nvim plugins

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -114,6 +114,83 @@ require("lazy").setup({
     { "nvim-lualine/lualine.nvim", dependencies = { 'nvim-tree/nvim-web-devicons' }},
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { "lewis6991/gitsigns.nvim" },
+    { -- LSP Configuration & Plugins
+      'neovim/nvim-lspconfig',
+      dependencies = {
+        -- Automatically install LSPs to stdpath for neovim
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+
+        -- Useful status updates for LSP
+        'j-hui/fidget.nvim',
+      }
+    },
+    { -- Autocompletion
+      'hrsh7th/nvim-cmp',
+      event = "InsertEnter",
+      dependencies = {
+        'hrsh7th/cmp-nvim-lsp',
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip'
+      },
+      config = function()
+        -- nvim-cmp setup
+        local cmp = require 'cmp'
+        local luasnip = require 'luasnip'
+
+        cmp.setup({
+          view = {
+            entries = "native"
+          },
+          snippet = {
+            expand = function(args)
+              luasnip.lsp_expand(args.body)
+            end,
+          },
+          mapping = cmp.mapping.preset.insert {
+            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<CR>'] = cmp.mapping.confirm {
+              behavior = cmp.ConfirmBehavior.Replace,
+              select = true,
+            },
+            ['<Tab>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              else
+                fallback()
+              end
+            end, { 'i', 's' }),
+            ['<S-Tab>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { 'i', 's' }),
+          },
+          sources = {
+            { name = 'nvim_lsp' },
+            { name = 'luasnip' },
+            { name = "neorg" },
+          },
+        })
+      end
+    },
+    {
+      'nvim-treesitter/nvim-treesitter',
+      build = function()
+        pcall(require('nvim-treesitter.install').update { with_sync = true })
+      end,
+      dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+      }
+    },
     {
       "nvim-telescope/telescope.nvim",
       dependencies = { 'nvim-lua/plenary.nvim' },
