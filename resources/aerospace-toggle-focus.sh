fafa3fe3e19d5324e82adb5a4da919b33067e869
itsAlexK <contactkaichen@gmail.com>
2025-01-27 03:26:06 -0500

    Add aerospace toggle cmd

---
+++ resources/aerospace-toggle-focus.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title aerospace toggle focus
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+aerospace focus-back-and-forth || aerospace workspace-back-and-forth
