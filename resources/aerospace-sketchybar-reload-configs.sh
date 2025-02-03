b47c0b369af25631da33065d452ebef523f7b8c3
itsAlexK <contactkaichen@gmail.com>
2025-02-03 14:49:06 -0500

    add reload aerospace and sketchybar configs

---
+++ resources/aerospace-sketchybar-reload-configs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title aerospace sketchybar reload configs
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+aerospace  reload-config
+sketchybar --reload
+
