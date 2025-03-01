cad4f31cc0b3b95c9a0f56b88917e1bc328088d8
itsAlexK <contactkaichen@gmail.com>
2025-03-01 15:26:35 -0500

    refactor(sketchybar): clock formatting

--- .config/sketchybar/items/calendar.lua
+++ .config/sketchybar/items/calendar.lua
@@ -5,6 +5,13 @@ local cal = sbar.add("item", {
         align = "right",
         padding_right = 4
     },
+    icon = {
+        font = {
+            size = 16.0
+        },
+        padding_left = settings.padding.icon_label_item.icon.padding_left,
+        padding_right = settings.padding.icon_label_item.icon.padding_right - 4
+    },
     position = "right",
     update_freq = 30,
     padding_left = 1,
@@ -13,6 +20,7 @@ local cal = sbar.add("item", {
 
 cal:subscribe({"forced", "routine", "system_woke"}, function(env)
     cal:set({
-        label = os.date("%A, %Y-%m-%d %H:%M")
+        label = os.date("%a, %Y-%m-%d %H:%M"),
+        icon = "󰸗"
     })
 end)
