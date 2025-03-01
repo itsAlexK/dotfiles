6cda5b66ea79a32003e83a3419a5eb99c33af101
itsAlexK <contactkaichen@gmail.com>
2025-03-01 01:06:27 -0500

    fix(sketchybar): change calendar format

--- .config/sketchybar/items/calendar.lua
+++ .config/sketchybar/items/calendar.lua
@@ -1,27 +1,18 @@
 local settings = require("settings")
 
 local cal = sbar.add("item", {
-    icon = {
-        font = {
-            family = settings.font.text,
-            style = settings.font.style_map["Regular"],
-            size = settings.font.size,
-        },
-        padding_left = 8,
-    },
     label = {
         align = "right",
-        font = {
-            size = 13,
-        },
-        padding_right = 10,
+        padding_right = 4
     },
     position = "right",
     update_freq = 30,
     padding_left = 1,
-    padding_right = 1,
+    padding_right = 1
 })
 
-cal:subscribe({ "forced", "routine", "system_woke" }, function(env)
-    cal:set({ icon = os.date("%A, %Y-%m-%d"), label = os.date("%H:%M") })
+cal:subscribe({"forced", "routine", "system_woke"}, function(env)
+    cal:set({
+        label = os.date("%A, %Y-%m-%d %H:%M")
+    })
 end)
